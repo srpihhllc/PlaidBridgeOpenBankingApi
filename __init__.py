2fb72f3c92861d9bd31d4e5d5c9317adb1b382f2
srpihhllc <srpollardsihhllc@gmail.com>
2024-11-02 14:30:35 -0500

    Create __init__.py
    Signed-off-by: srpihhllc <srpollardsihhllc@gmail.com>

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# PlaidBridgeOpenBankingApi/__init__.py
+
+# Import specific modules or classes
+from .example_module import ExampleClass
+
+# Initialize variables or settings
+__version__ = '0.1.0'
+
+# Any other setup code
