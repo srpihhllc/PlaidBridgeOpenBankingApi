ab82393d75dc5a5bd150860abb17104b84f6263d
srpihhllc <srpollardsihhllc@gmail.com>
2025-02-07 17:50:30 -0600

    Update example_module.py
    Signed-off-by: srpihhllc <srpollardsihhllc@gmail.com>

--- subdir/example_module.py
+++ subdir/example_module.py
@@ -1,6 +1,9 @@
+# example_module.py
+
 class ExampleClass:
     def __init__(self):
         pass
 
     def example_method(self):
-        print("This is an example method")
+        return "Hello from ExampleClass"
+
